fa69de18a7fef8a9c53c3d123d5327e1dd3c29f2
Prince J Wesley <princejohnwesley@gmail.com>
2016-02-29 18:50:58 +0530

    :bug: rebuild ignored without error

--- src/spawn.js
+++ src/spawn.js
@@ -4,12 +4,15 @@ import childProcess from 'child_process';
 export default function (options={}) {
   const stdout = (options.stdout && process.stdout) ? process.stdout : [];
   const stderr = (options.stderr && process.stderr) ? process.stderr : [];
-  
+
   return new Promise((resolve, reject) => {
     let error = null;
     //console.log("Calling spawn! " + JSON.stringify(options));
+    if (!('cwd' in options.opts)) {
+      options.opts.cwd = process.cwd() + '/node_modules/electron-prebuilt';
+    }
     let proc = childProcess.spawn(options.cmd, options.args, options.opts);
-    
+
     proc.stdout.on('data', (data) => {
       if(_.isArray(stdout)) {
         stdout.push(data.toString());
@@ -17,7 +20,7 @@ export default function (options={}) {
         stdout.write(data.toString());
       }
     });
-        
+
     proc.stderr.on('data', (data) => {
       if(_.isArray(stderr)) {
         stderr.push(data.toString());
@@ -25,24 +28,24 @@ export default function (options={}) {
         stderr.write(data.toString());
       }
     });
-        
+
     proc.on('error', (processError) => error = error || processError);
-    
+
     proc.on('close', (exitCode, signal) => {
       let stdoutStr = (_.isArray(stdout) ? stdout.join('') : '');
       let stderrStr = (_.isArray(stderr) ? stderr.join('') : '');
-      
+
       if (exitCode !== 0) {
         error = error || new Error("Process exited with code: " + exitCode);
         error.stdout = stdoutStr;
         error.stderr = stderrStr;
       }
-      
+
       let results = {
         stderr: stderrStr, stdout: stdoutStr,
         code: exitCode
       };
-      
+
       if (error) { reject(error); } else { resolve(results); }
     });
   });
